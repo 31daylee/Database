f2296855ff8b236e347a37329472fe30c36a93fc
gussla2206 <gussla2206@gmail.com>
2023-08-08 11:19:10 +0900

    게시판 실습 중...

--- 프로젝트 실습_게시판.sql
+++ 프로젝트 실습_게시판.sql
@@ -49,3 +49,25 @@ CREATE TABLE `Terms`(
 	`terms` 		TEXT NOT NULL,
 	`privacy`	TEXT NOT NULL
 );
+
+
+ALTER TABLE `Article` AUTO_INCREMENT=1;
+
+SELECT  
+	a.*,
+	b.`nick`
+FROM `Article` AS a
+JOIN `User` AS b 
+ON a.writer = b.uid
+ORDER BY `no` DESC
+LIMIT 0, 10;
+
+
+#게시물채우기
+INSERT INTO `Article`(`title`, `content`,`writer`,`regip`, `rdate`) 
+SELECT `title`, `content`,`writer`,`regip`, `rdate` FROM `Article`;
+
+
+# 전체 게시물 갯수
+SELECT COUNT(*) FROM `Article`;
+
